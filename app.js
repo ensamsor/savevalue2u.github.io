61a0c393b6ad36b0698744dd92deb027ab99869d
ensamsor <ensamsor@gmail.com>
2020-07-03 04:35:18 +0800

    kkkk

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.getElementById('myform').addEventListener('submit', function(e){
     let strProductId = newProductId.toString().replace(/\,/g,"");
 
     let unsyncProduct = "https://www.savevalue2u.com.my/shop-sync/shopee_product_function.php?remove=1&product_id=g" + strProductId;
-    window.open(unsyncProduct);
+    location.replace(unsyncProduct);
 
 });
 
