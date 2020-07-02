475170bbf6a3c0471477f666f8c5907018668746
ensamsor <ensamsor@gmail.com>
2020-07-03 04:14:11 +0800

    woiii

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ document.getElementById('myform').addEventListener('submit', function(e){
     let strProductId = newProductId.toString().replace(/\,/g,"");
 
     setTimeout(function(){
-        let removeProduct = "https://www.savevalue2u.com.my/shop-sync/shopee_product_function.php?remove=1&product_id=g" + strProductId;
-        window.open(removeProduct);
+        let removeProduct = "https://www.savevalue2u.com.my/shop-sync/sync_product.php?action=delete&product_id=g" + strProductId;
+        window.open(removeProduct,"_blank");
         setTimeout(function(){
-            let unsyncProduct = "https://www.savevalue2u.com.my/shop-sync/sync_product.php?action=delete&product_id=g" + strProductId;
+            let unsyncProduct = "https://www.savevalue2u.com.my/shop-sync/shopee_product_function.php?remove=1&product_id=g" + strProductId;
             window.open(unsyncProduct,"_blank");
         },1);
     },2);
