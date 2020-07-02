9b2e6c8fcbe40d56e7d16f508bf4785acb856819
ensamsor <ensamsor@gmail.com>
2020-07-03 02:32:08 +0800

    ccccc

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById('myform').addEventListener('submit', function(e){
     e.preventDefault();
-    
-    let success = document.getElementById('success');
+    let success = "";
+    document.getElementById('success').innerHTML = success;
     let getUrl = document.getElementById('product_url').value;
     let splitUrl = getUrl.split('=');
     let productId = splitUrl[1].trim();
@@ -11,12 +11,13 @@ document.getElementById('myform').addEventListener('submit', function(e){
     setTimeout(function(){
         let removeProduct = "https://www.savevalue2u.com.my/shop-sync/shopee_product_function.php?remove=1&product_id=g" + strProductId;
         location.replace(removeProduct);
+        success = "success";
         setTimeout(function(){
             let unsyncProduct = "https://www.savevalue2u.com.my/shop-sync/sync_product.php?action=delete&product_id=g" + strProductId;
             location.replace(unsyncProduct);
-            // success.innerHTML="Success";
+            success = "suuuccess";
         },1);
-        success.innerHTML="Success";
     },2);
+    success = "suucesssss";
 });
 
